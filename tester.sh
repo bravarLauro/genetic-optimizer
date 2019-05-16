69c5b231525bb2bafb688abcea774de260aa684c
Lauro Bravar <bravar.lauro@gmail.com>
2019-05-16 22:21:58 +0200

    Added test cases and results. Created bash file for testing

---
+++ tester.sh
@@ -0,0 +1,23 @@
+#### 10000 iterations, cross. prob. of 0.2,  max cap. of 500
+### Tests for 80 points
+## Tests for gen. size of 100
+# Test for mut. prob. of 0.1
+python genetic.py 100 10000 0.1 0.2 500 80 -c test_files/prueba_80
+# Test for mut. prob. of 0.2
+python genetic.py 100 10000 0.2 0.2 500 80 -c test_files/prueba_80
+## Tests for gen. size of 400
+# Test for mut. prob. of 0.1
+python genetic.py 400 10000 0.1 0.2 500 80 -c test_files/prueba_80
+# Test for mut. prob. of 0.2
+python genetic.py 400 10000 0.2 0.2 500 80 -c test_files/prueba_80
+# Tests for 500 points
+## Tests for gen. size of 100
+# Test for mut. prob. of 0.1
+python genetic.py 100 10000 0.1 0.2 500 500 -c test_files/prueba_500
+# Test for mut. prob. of 0.2
+python genetic.py 100 10000 0.2 0.2 500 500 -c test_files/prueba_500
+## Tests for gen. size of 400
+# Test for mut. prob. of 0.1
+python genetic.py 400 10000 0.1 0.2 500 500 -c test_files/prueba_500
+# Test for mut. prob. of 0.2
+python genetic.py 400 10000 0.2 0.2 500 500 -c test_files/prueba_500
